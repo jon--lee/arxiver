fae87e12e0511ad0fb6d7c4b90878c27c61bda48
Jonathan Lee <123abcjonathanlee@gmail.com>
2015-07-25 20:41:17 -0700

    switch directories, add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+setup(
+        name ='arxiver',
+        version='0.0.1',
+        description='unofficial API tool for arxiv.org scientific publications',
+        author='Jonathan N. Lee'
+        author_email='jonathan_lee@berkeley.edu'.
+        url='https://github.com/jon--lee/arxiver',
+        license='MIT',
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Development Status :: 3 - Alpha',
+	    'Intended Audience :: Developers',
+            'Intended Audience :: Science/Research',
+    	    'Topic :: Software Development',
+	    'Topic :: Scientific/Engineering',
+	    'Programming Language :: Python',
+	    'Programming Language :: Python :: 2.7',
+            ],
+        install_requirements['beautifulsoup4']
+        packages=find_packages()
+)
